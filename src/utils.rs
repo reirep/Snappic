3a854471700291fd2bbb1e4c8a82c0b72b270db1
Pierre Ortegat <pierre.ortegat@student.uclouvain.be>
2018-04-01 12:35:21 +0200

    added convolution function

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use raster::{Image,Color};
+
 pub fn add(base : u8, change : u8) -> u8
 {
     match base.checked_add(change) {
@@ -20,4 +22,57 @@ pub fn limit(nbr : f32) -> u8
     if value < 0.0 {0}
     else if value > 255.0 {255}
     else {nbr as u8}
+}
+
+//les dimensions de kernel doivent être impaires
+pub fn convolution(img: &mut Image, mult: f64, kernel : &Vec<Vec<f64>>)
+{
+    for y in 0..img.height {
+        for x in 0..img.width {
+            let mut r : f64 = 0.0;
+            let mut g : f64 = 0.0;
+            let mut b : f64 = 0.0;
+
+            for kx in 0..kernel.len() {
+                for ky in 0..kernel[kx as usize].len()  {
+                    let ax : i32 = x - (kernel.len()/2 + kx) as i32;
+                    let ay : i32 = y - (kernel[ax as usize].len()/2 + ky) as i32;
+                    
+                    let color = get_px(img, ax, ay);
+
+                    r += color.r as f64 * kernel[kx][ky];
+                    g += color.g as f64 * kernel[kx][ky];
+                    b += color.b as f64 * kernel[kx][ky];
+
+                }
+            }  
+
+            r*=mult;
+            g*=mult;
+            b*=mult;
+
+            img.set_pixel(x,y,Color::rgb(r as u8, g as u8, b as u8)).unwrap();
+        }
+    }
+}
+
+//we use the mirror technique to get the pixel out of the picture for the convolution
+fn get_px(img: &Image, x : i32, y : i32) -> Color 
+{
+    let real_x = inside_range(img.width, x);
+    let real_y = inside_range(img.height, y);
+    
+    img.get_pixel(real_x, real_y).unwrap()
+}
+
+fn inside_range(max : i32, value : i32) -> i32
+{
+    let mut result = value;
+    if result > max {
+        result *= -1;
+    } 
+    while result < 0 {
+        result += max;
+    }
+    result
 }
